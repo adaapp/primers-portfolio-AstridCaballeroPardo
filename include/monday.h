d90c6ed894bf34e0cdd6478029cd4cd50336d42e
Astrid Caballero-Pardo <89023220+AstridCaballeroPardo@users.noreply.github.com>
2021-08-17 16:59:30 +0000

    Primer1 major refactoring

--- include/monday.h
+++ include/monday.h
@@ -1,3 +1,6 @@
+/*
+  functions used by fahrenheitCentigradeConversion()
+*/
 float ftoc(float f) {
   return ((f - 32) * 5 / 9); //Fahrenheit to Centigrade.
 }
@@ -22,37 +25,50 @@ float ktof(float k) {
   return ((k * 9 / 5) - 459.67); //Centigrade to Fahrenheit.  
 }
 
-float tempInput(){
-  std::string temp;
-  std::getline(std::cin, temp);
-  return stof(temp);
+//helper function
+std::string userInput(std::string prompt) {
+  std::string reply = "";
+  std::cout << prompt;  
+  std::getline(std::cin, reply);
+  return reply;
+}
+
+//helper function
+bool isNumber(std::string str) {
+  bool state = true;
+  //pattern to check if input string matches numbers
+  std::regex regex_pattern(R"([-]?((\d+(\.\d+)?)|(\d+\.)|(\.\d+))(e[-+]?\d+)?*$)");
+  if (!std::regex_match(str,regex_pattern)) {
+    state = false;
+  }  
+  return state;
+}
+
+//helper function
+float stringToFloat(std::string str, float deflt = -1) {
+  float v = deflt;
+  if (isNumber(str)) {
+    v = stof(str);
+  }  
+  return v;
 }
 
 void fahrenheitCentigradeConversion(void) {
 	std::string temp;
   std::string label;
+  std::string c = "";
   char choice;
   float ot = 0.0;
   float t = 0.0;
 
-  std::cout << "\nPlease enter the starting temperature: ";
-  try {
-    ot = tempInput();
-  }
-  catch(...) {
-    std::cout << "You must enter a number.\n";
-    return;
-  } 
-
-  std::cout << "\nPress ‘C’ to convert from Fahrenheit to Centigrade.";
-  std::cout << "\nPress ‘F’ to convert from Centigrade to Fahrenheit.";
-  std::cout << "\nPress ‘K’ to convert from Centigrade to Kelvin.";
-  std::cout << "\nPress ‘G’ to convert from Kelvin to Centigrade.";
-  std::cout << "\nPress ‘D’ to convert from Fahrenheit to Kelvin";
-  std::cout << "\nPress ‘H’ to convert from Kelvin to Fahrenheit.\n\n";
-
-  std::cout << "\nYour choice: ";
-  std::cin >> choice;
+  //ot = stringToFloat(userInput("\nPlease enter the starting temperature: "));
+  do {
+    ot = stringToFloat(userInput("\nPlease enter the starting temperature: "));
+  } while (ot == -1);
+  
+  c = userInput("\nPress ‘C’ to convert from Fahrenheit to Centigrade.\nPress ‘F’ to convert from Centigrade to Fahrenheit.\nPress ‘K’ to convert from Centigrade to Kelvin.\nPress ‘G’ to convert from Kelvin to Centigrade.\nPress ‘D’ to convert from Fahrenheit to Kelvin.\nPress ‘H’ to convert from Kelvin to Fahrenheit.\n\nYour choice: ");
+ 
+  choice = c[0];
 
   switch(tolower(choice)) {
     case 'c': 
@@ -96,10 +112,13 @@ void fahrenheitCentigradeConversion(void) {
 /*
   functions used by selfServiceCheckout()
 */
+
+//Function that calculates the cost of a number of same items
 float costItems(int numberItems, float valueItem) {
   return numberItems * valueItem;
 }
 
+//Function that calculates the tax based on the subtotal
 float taxCal(float subtotal) {
   const float tax = 5.5;
   return (subtotal * tax) / 100;
