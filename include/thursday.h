30da4d33f10fb1e9980b8c9586359ada62dd91ad
Astrid Caballero-Pardo <89023220+AstridCaballeroPardo@users.noreply.github.com>
2021-08-20 11:45:05 +0000

    Primer7 changed my mind on the way to solve the primer

--- include/thursday.h
+++ include/thursday.h
@@ -1,17 +1,6 @@
-/*
-  functions used by sleepTimer()
-*/
-
-void sleepnow(int seconds = 10) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));
+void sleepTimer(int seconds = 10) {
+	std::this_thread::sleep_for(std::chrono::milliseconds(seconds * 1000));	
 }
-
-void sleepTimer(void) {
-	std::cout << "start\n";
-	sleepnow();
-  std::cout << "end\n";
-}
-
 
 void joinDetachThreads(void) {
 	std::cout << " - joinDetachThreads: not yet implemented\n\n";
